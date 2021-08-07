cbae57ad7333b8743f5f9ebdb1a69f9e38f67de9
Nekoshita Yuki <yuki.303442@gmail.com>
2021-08-07 21:12:30 +0900

    fix: nextjs pages must represent routing

--- web/pages/dividend_history/index.tsx
+++ web/pages/dividend_history/index.tsx
@@ -1,30 +1,3 @@
-import * as React from "react";
-import { useRouter } from "next/router";
-
-import { DividendHistoryChart } from "./dividend_history_chart";
-import { TickerForm } from "./ticker_form";
-
-const DividendHistory = () => {
-  const router = useRouter();
-  const q = router.query;
-  const [ticker, setTicker] = React.useState<string | undefined>(undefined);
-
-  React.useEffect(() => {
-    if (typeof q.ticker === "undefined") {
-      setTicker(undefined);
-      return;
-    }
-    setTicker(Array.isArray(q.ticker) ? q.ticker[0] : q.ticker);
-  }, [q.ticker]);
-
-  return (
-    <>
-      <TickerForm ticker={ticker} />
-      {typeof ticker !== "undefined" && (
-        <DividendHistoryChart ticker={ticker} />
-      )}
-    </>
-  );
-};
+import { DividendHistory } from "src/components/pages/dividend_history";
 
 export default DividendHistory;
